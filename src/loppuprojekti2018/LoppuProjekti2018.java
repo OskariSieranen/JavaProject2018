4e1545abb52c568bd7f392caccef8cba0f5478f5
Oskari Sieranen <oskari.sieranen@gmail.com>
2018-02-14 16:39:18 +0200

    Initial commit

---
+++ src/loppuprojekti2018/LoppuProjekti2018.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loppuprojekti2018;
+
+/**
+ *
+ * @author aamu
+ */
+public class LoppuProjekti2018 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("lol");
+    }
+    
+}
