05d83c9b6a560b37f6a3eb012ca9bd040c4c8524
Oskari Sieranen <oskari.sieranen@gmail.com>
2018-02-15 10:05:57 +0200

    TestiPoisto

--- src/loppuprojekti2018/LoppuProjekti2018.java
+++ src/loppuprojekti2018/LoppuProjekti2018.java
@@ -8,7 +8,6 @@ package loppuprojekti2018;
 /**
  * This is the class for the user side of this program.
  * This will only interact with the Main Controller Class.
- * lolollolo
  */
 public class LoppuProjekti2018 {
     /**
@@ -16,8 +15,6 @@ public class LoppuProjekti2018 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("lol");
-        
     }
     
 }
