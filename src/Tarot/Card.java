a424609c12e39ddce989b7c86c9f43bd0553c94d
OskariSieranen <oskari.sieranen@gmail.com>
2018-02-25 20:06:12 +0200

    Tarot Deck and Card classes created

---
+++ src/Tarot/Card.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tarot;
+
+/**
+ * The Tarot card object
+ * @author aamu
+ */
+public class Card {
+    String name, number, description;
+    
+    Card(String name, String number, String description) {
+        this.name = name;
+        this.number = number;
+        this.description = description;
+    }
+}
---
+++ src/Tarot/TarotDeck.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tarot;
+/**
+ * A list of Tarot cards.
+ * @author aamu
+ */
+public class TarotDeck {
+    private Card[] deck =  new Card[] {
+        new Card("The Fool", "0", "Lol"),
+        new Card("The Magician", "I", "lol"),
+        new Card("The High Priestess", "II", "lol"),
+        new Card("The Empress", "III", "lol"),
+        new Card("The Emperor", "IV", "lol"),
+        new Card("The Hierophant", "V", "lol"),
+        new Card("The Lovers", "VI", "lol"),
+        new Card("The Chariot", "VII", "lol"),
+        new Card("Strength", "VIII", "lol"),
+        new Card("The Hermit", "IX", "lol"),
+        new Card("Wheel of Fortune", "X", "lol"),
+        new Card("Justice", "XI", "lol"),
+        new Card("The Hanged Man", "XII", "lol"),
+        new Card("Death", "XIII", "lol"),
+        new Card("Temperance", "XIV", "lol"),
+        new Card("The Devil", "XV", "LOL"),
+        new Card("The Tower", "XVI", "lol"),
+        new Card("The Star", "XVII", "lol"),
+        new Card("The Moon", "XVIII", "lol"),
+        new Card("The Sun", "XIX", "lol"),
+        new Card("Judgement", "XX", "lol"),
+        new Card("The World", "XXI", "Lol")
+    };
+}
