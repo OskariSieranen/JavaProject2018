97f4eca314f5831626cd2ea006a8cfa48197da45
OskariSieranen <oskari.sieranen@gmail.com>
2018-02-27 21:13:40 +0200

    Added the meanings to Tarot cards.

--- src/Tarot/TarotDeck.java
+++ src/Tarot/TarotDeck.java
@@ -7,30 +7,32 @@ package Tarot;
 /**
  * A list of Tarot cards.
  * @author aamu
- */
+ * Source for most of the meanings: https://www.biddytarot.com/tarot-card-meanings/major-arcana/
+*/
+
 public class TarotDeck {
     private Card[] deck =  new Card[] {
-        new Card("The Fool", "0", "Lol"),
-        new Card("The Magician", "I", "lol"),
-        new Card("The High Priestess", "II", "lol"),
-        new Card("The Empress", "III", "lol"),
-        new Card("The Emperor", "IV", "lol"),
-        new Card("The Hierophant", "V", "lol"),
-        new Card("The Lovers", "VI", "lol"),
-        new Card("The Chariot", "VII", "lol"),
-        new Card("Strength", "VIII", "lol"),
-        new Card("The Hermit", "IX", "lol"),
-        new Card("Wheel of Fortune", "X", "lol"),
-        new Card("Justice", "XI", "lol"),
-        new Card("The Hanged Man", "XII", "lol"),
-        new Card("Death", "XIII", "lol"),
-        new Card("Temperance", "XIV", "lol"),
-        new Card("The Devil", "XV", "LOL"),
-        new Card("The Tower", "XVI", "lol"),
-        new Card("The Star", "XVII", "lol"),
-        new Card("The Moon", "XVIII", "lol"),
-        new Card("The Sun", "XIX", "lol"),
-        new Card("Judgement", "XX", "lol"),
-        new Card("The World", "XXI", "Lol")
+        new Card("The Fool", "0", "Beginnings | Innocence | Spontaneity"),
+        new Card("The Magician", "I", "Power | Skill | Action"),
+        new Card("The High Priestess", "II", "Intuition | Higher Powers | Mystery"),
+        new Card("The Empress", "III", "Fertility | Beauty | Nature"),
+        new Card("The Emperor", "IV", "Authority | Structure | Solid Foundation"),
+        new Card("The Hierophant", "V", "Religion | Beliefs | Tradition"),
+        new Card("The Lovers", "VI", "Love | Union | Choices"),
+        new Card("The Chariot", "VII", "Control | Will Power | Victory"),
+        new Card("Strength", "VIII", "Strength | Patience | Control"),
+        new Card("The Hermit", "IX", "Introspection | Being Alone | Inner Guidance"),
+        new Card("Wheel of Fortune", "X", "Destiny | Turning Point | Good Luck"),
+        new Card("Justice", "XI", "Justice | Fairness | Truth"),
+        new Card("The Hanged Man", "XII", "Sacrifice | Letting Go | Suspension"),
+        new Card("Death", "XIII", "Transformation | Endings | Change"),
+        new Card("Temperance", "XIV", "Balance | Moderation | Purpose"),
+        new Card("The Devil", "XV", "Addicition | Materialism | Sexuality"),
+        new Card("The Tower", "XVI", "Disaster | Revelation | Sudden Change"),
+        new Card("The Star", "XVII", "Hope | Renewal | Serenity"),
+        new Card("The Moon", "XVIII", "Illusion | Fear | Subconcious"),
+        new Card("The Sun", "XIX", "Warmth | Positivity | Vitality"),
+        new Card("Judgement", "XX", "Absolution | Rebirth | Inner Calling"),
+        new Card("The World", "XXI", "Completion | Journey | Accomplishment")
     };
 }
