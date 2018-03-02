95d1582ee7edbb8063942e5b32d049028587658c
OskariSieranen <oskari.sieranen@gmail.com>
2018-03-02 13:16:14 +0200

    Added Controller and UI classes

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,18 +1,22 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor. 
  */ 
 package Controller;
-
+import Blackjack.BlackJackGame;
+import Tarot.TarotDeck;
+import javax.swing.JOptionPane;
 /**
- * This is the class for the user side of this program.
- * This will only interact with the Main Controller Class.
+ * Controller class for the games found within.
+ * 
  */
 public class Controller {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        BlackJackGame BJG = new BlackJackGame();
+        
+        
     }
     
 }
