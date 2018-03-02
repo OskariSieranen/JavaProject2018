02ca4569b9eefbf48c1c782e3325a6ba5da016c8
OskariSieranen <oskari.sieranen@gmail.com>
2018-03-02 12:41:09 +0200

    Changed Controller class naming

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -3,19 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor. 
  */ 
-package loppuprojekti2018;
+package Controller;
 
 /**
  * This is the class for the user side of this program.
  * This will only interact with the Main Controller Class.
  */
-public class LoppuProjekti2018 {
-    /**
-     * @param args the command line arguments
-     */
+public class Controller {
+
     public static void main(String[] args) {
         // TODO code application logic here
-        double säde;
     }
     
 }
